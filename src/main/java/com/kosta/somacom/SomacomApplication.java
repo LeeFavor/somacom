352c876104fb78f9a928947c193837cbb97a5837
LeeFavor <ljh001011@gmail.com>
2025-12-02 11:46:30 +0900

    #1.11.3

--- src/main/java/com/kosta/somacom/SomacomApplication.java
+++ src/main/java/com/kosta/somacom/SomacomApplication.java
@@ -1,18 +1,14 @@
 package com.kosta.somacom;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"com.kosta.somacom.domain"}) // [수정] 모든 엔티티 패키지를 스캔하도록 설정
 public class SomacomApplication {
 
-	// 임시 테스트용 코드 (예: SomacomApplication.java에 잠시 추가)
 	public static void main(String[] args) {
-//	    BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-//	    System.out.println(passwordEncoder.encode("password123"));
-		SpringApplication.run(SomacomApplication.class, args); // 잠시 주석 처리
+		SpringApplication.run(SomacomApplication.class, args);
 	}
-
-
 }
